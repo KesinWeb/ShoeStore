7324cf817b1ce22804f3a9e1197fa083c7002c1d
KesinWeb <keyjo2018@gmail.com>
2026-01-18 05:13:41 +0300

    фывфы

--- ShoeStore/Forms/MainForm.cs
+++ ShoeStore/Forms/MainForm.cs
@@ -221,12 +221,5 @@ namespace ShoeStore
             this.flowLayoutPanel1.Controls.Clear();
             this.flowLayoutPanel1.Controls.Add(addZakazPanelRedact);  
         }
-
-        private void buttonBack_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            AutForm autForm = new AutForm();
-            autForm.ShowDialog();
-        }
     }  
 }
